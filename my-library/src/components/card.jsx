ffb566af3e773c0fd912639f49471abdf2ccc640
Kyle Duong <kyleduong13@gmail.com>
2024-01-21 15:32:30 -0700

    Created cards for the books

---
+++ my-library/src/components/card.jsx
@@ -0,0 +1,17 @@
+import './card.css'
+
+function card(props) {
+    return (
+        <div className="card" style={{
+            backgroundImage: `url("${props.url}")`,
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            backgroundRepeat: 'no-repeat'
+        }}>
+
+            <h3>{props.title}</h3>
+        </div>
+    );
+}
+
+export default card;
--- my-library/src/index.js
+++ my-library/src/index.js
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Card from './components/card';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Card title="Hello" url="https://picsum.photos/200/400"/>
+    <Card title="World" url="https://picsum.photos/200/200"/>
+    <Card title="Image" url="https://picsum.photos/200/300"/>
   </React.StrictMode>
 );
 
